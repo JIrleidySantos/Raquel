49afc484cf142daaf96cab59a0039727eb57a2f6
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-05 09:35:54 -0500

    defensa

---
+++ src/main/java/ec/edu/espoch/defensa/Defensa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.defensa;
+
+/**
+ *
+ * @author mundo
+ */
+public class Defensa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
